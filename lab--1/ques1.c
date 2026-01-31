b82855a70348b38065ba649e786138cf8251aad1
Deepanshishukla2009 <shuklashiri154@gmail.com>
2026-01-31 14:58:27 +0530

    daily lab ques

---
+++ lab--1/ques1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<time.h>
+int main()
+{ int n;
+    printf("enter n:");
+    scanf("%d",&n);
+    clock_t start = clock();
+for(int i=1;i<=n;i++)
+{
+    for(int j=1;j<=i;j++)
+    {
+        for(int k=1;k<=100;k++)
+        {
+            printf("UPES");
+        }
+    }
+}
+clock_t end = clock();
+ double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Execution time = %f seconds\n", time_taken);
+
+    return 0;
+}
