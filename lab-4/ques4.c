b5f7c4acad7185399dfc41fb161937aa88772204
Deepanshishukla2009 <shuklashiri154@gmail.com>
2026-02-12 08:37:17 +0530

    daily lab ques

---
+++ lab-4/ques4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Employee {
+    int id;
+    char name[50];
+    float salary;
+};
+
+int main() {
+    struct Employee emp;
+    struct Employee *p = &emp;
+
+    printf("Enter Employee ID: ");
+    scanf("%d", &p->id);
+
+    printf("Enter Employee Name: ");
+    scanf("%s", p->name);
+
+    printf("Enter Employee Salary: ");
+    scanf("%f", &p->salary);
+
+    printf("\nEmployee Details:\n");
+    printf("ID: %d\n", p->id);
+    printf("Name: %s\n", p->name);
+    printf("Salary: %.2f\n", p->salary);
+
+    printf("\nAddresses of Employee Members:\n");
+    printf("Address of ID: %p\n", (void*)&p->id);
+    printf("Address of Name: %p\n", (void*)&p->name);
+    printf("Address of Salary: %p\n", (void*)&p->salary);
+
+    return 0;
+}
