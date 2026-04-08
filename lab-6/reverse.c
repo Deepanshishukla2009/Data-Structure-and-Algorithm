9bc85604c1988e8b1c930f8454b5e40d9bdd5033
Deepanshishukla2009 <shuklashiri154@gmail.com>
2026-04-08 19:45:42 +0530

    daily

--- lab-6/reverse.c
+++ lab-6/reverse.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct Node
 {
     int data;
     struct Node *next;
 };
-
 struct Node *head = NULL;
 void createList()
 {
     int n, val, i;
     struct Node *temp, *newNode;
-
     printf("Enter number of nodes: ");
     scanf("%d", &n);
-
     if(n == 0)
         return;
-
     for(i = 1; i <= n; i++)
     {
         printf("Enter value %d: ", i);
         scanf("%d", &val);
-
         newNode = (struct Node*)malloc(sizeof(struct Node));
         newNode->data = val;
         newNode->next = NULL;
-
         if(head == NULL)
         {
             head = newNode;
@@ -37,7 +30,6 @@ void createList()
             temp = head;
             while(temp->next != NULL)
                 temp = temp->next;
-
             temp->next = newNode;
         }
     }
@@ -45,13 +37,11 @@ void createList()
 void display()
 {
     struct Node *temp = head;
-
     if(head == NULL)
     {
         printf("List is empty\n");
         return;
     }
-
     while(temp != NULL)
     {
         printf("%d -> ", temp->data);
@@ -59,13 +49,11 @@ void display()
     }
     printf("NULL\n");
 }
-
 void reverse()
 {
     struct Node *prev = NULL;
     struct Node *curr = head;
     struct Node *next = NULL;
-
     while(curr != NULL)
     {
         next = curr->next;
@@ -73,10 +61,8 @@ void reverse()
         prev = curr;
         curr = next;
     }
-
     head = prev;
 }
-
 int main()
 {
     createList();
