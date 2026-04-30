3bac948b334c3ad17f0eb2b2f63f302ba9bce69a
Deepanshishukla2009 <shuklashiri154@gmail.com>
2026-04-30 08:08:16 +0530

    daily

--- lab-10/ques-1.c
+++ lab-10/ques-1.c
@@ -1,96 +1,126 @@
-/*BST using array*/
+/*Adjacency matrix and adjacency list of a graph*/
 
 #include <stdio.h>
-#define SIZE 100
+#include <stdlib.h>
 
-int tree[SIZE];  
+#define MAX 10
 
-void init() {
-    for (int i = 0; i < SIZE; i++) {
-        tree[i] = -1;
+// ----------- Adjacency Matrix -----------
+int matrix[MAX][MAX];
+int vertices;
+
+// ----------- Adjacency List -----------
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* adjList[MAX];
+
+// Create new node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Add edge to list
+void addEdgeList(int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = adjList[src];
+    adjList[src] = newNode;
+}
+
+// Create Graph (both matrix & list)
+void createGraph() {
+    int edges, src, dest;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &vertices);
+
+    // Initialize matrix and list
+    for (int i = 0; i < vertices; i++) {
+        adjList[i] = NULL;
+        for (int j = 0; j < vertices; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+
+    printf("Enter number of edges: ");
+    scanf("%d", &edges);
+
+    for (int i = 0; i < edges; i++) {
+        printf("Enter edge (src dest): ");
+        scanf("%d %d", &src, &dest);
+
+        // Matrix
+        matrix[src][dest] = 1;
+
+        // List
+        addEdgeList(src, dest);
     }
 }
 
-void insert(int value, int index) {
-    if (index >= SIZE) {
-        printf("Tree is full\n");
-        return;
-    }
-
-    if (tree[index] == -1) {
-        tree[index] = value;
-        return;
-    }
-
-    if (value < tree[index]) {
-        insert(value, 2 * index + 1);   
-    } else {
-        insert(value, 2 * index + 2);   
+// Display Matrix
+void displayMatrix() {
+    printf("\nAdjacency Matrix:\n");
+    for (int i = 0; i < vertices; i++) {
+        for (int j = 0; j < vertices; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
     }
 }
 
-
-void inorder(int index) {
-    if (index >= SIZE || tree[index] == -1)
-        return;
-
-    inorder(2 * index + 1);
-    printf("%d ", tree[index]);
-    inorder(2 * index + 2);
-}
-
-
-void preorder(int index) {
-    if (index >= SIZE || tree[index] == -1)
-        return;
-
-    printf("%d ", tree[index]);
-    preorder(2 * index + 1);
-    preorder(2 * index + 2);
-}
-
-
-void postorder(int index) {
-    if (index >= SIZE || tree[index] == -1)
-        return;
-
-    postorder(2 * index + 1);
-    postorder(2 * index + 2);
-    printf("%d ", tree[index]);
-}
-
-void display() {
-    printf("\nArray Representation:\n");
-    for (int i = 0; i < 15; i++) {
-        if (tree[i] == -1)
-            printf("NULL ");
-        else
-            printf("%d ", tree[i]);
+// Display List
+void displayList() {
+    printf("\nAdjacency List:\n");
+    for (int i = 0; i < vertices; i++) {
+        printf("%d -> ", i);
+        struct Node* temp = adjList[i];
+        while (temp != NULL) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
     }
-    printf("\n");
 }
 
+// ----------- MAIN MENU -----------
 int main() {
-    init();
+    int choice;
 
-    insert(50, 0);
-    insert(30, 0);
-    insert(70, 0);
-    insert(20, 0);
-    insert(40, 0);
-    insert(60, 0);
-    insert(80, 0);
+    while (1) {
+        printf("\n--- GRAPH MENU ---\n");
+        printf("1. Create Graph\n");
+        printf("2. Display Adjacency Matrix\n");
+        printf("3. Display Adjacency List\n");
+        printf("4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
 
-    printf("Inorder: ");
-    inorder(0);
+        switch (choice) {
+            case 1:
+                createGraph();
+                break;
 
-    printf("\nPreorder: ");
-    preorder(0);
+            case 2:
+                displayMatrix();
+                break;
 
-    printf("\nPostorder: ");
-    postorder(0);
+            case 3:
+                displayList();
+                break;
 
-    display();
+            case 4:
+                printf("Exiting...\n");
+                exit(0);
+
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
 
     return 0;
 }
---
+++ lab-9/ques-3.c
@@ -0,0 +1,127 @@
+/*Insertion and deletion in BST*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Find minimum
+struct Node* findMin(struct Node* root) {
+    while (root->left != NULL)
+        root = root->left;
+    return root;
+}
+
+// Delete
+struct Node* deleteNode(struct Node* root, int key) {
+    if (root == NULL) {
+        printf("Element not found!\n");
+        return root;
+    }
+
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+
+    else {
+        // Case 1 & 2
+        if (root->left == NULL) {
+            struct Node* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            struct Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        // Case 3
+        struct Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+// Inorder traversal
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+// Main menu
+int main() {
+    struct Node* root = NULL;
+    int choice, value;
+
+    while (1) {
+        printf("\n--- BST MENU ---\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Display (Inorder)\n");
+        printf("4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                root = insert(root, value);
+                break;
+
+            case 2:
+                printf("Enter value to delete: ");
+                scanf("%d", &value);
+                root = deleteNode(root, value);
+                break;
+
+            case 3:
+                printf("BST (Inorder): ");
+                inorder(root);
+                printf("\n");
+                break;
+
+            case 4:
+                printf("Exiting...\n");
+                exit(0);
+
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+
+    return 0;
+}
